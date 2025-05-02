15cd673f406f10bd0ed6a6a7ba655c67f282d0a3
agusNazer <agustin.nazer@hotmail.com>
2025-05-02 19:45:14 -0300

    added config .env

--- src/main/java/com/agusdev/products/ProductsApplication.java
+++ src/main/java/com/agusdev/products/ProductsApplication.java
@@ -1,5 +1,6 @@
 package com.agusdev.products;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ProductsApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		System.setProperty("SPRING_DATASOURCE_DRIVER_CLASS_NAME", dotenv.get("SPRING_DATASOURCE_DRIVER_CLASS_NAME"));
+		// Verificar que se cargó correctamente
+		System.out.println(dotenv.get("SPRING_DATASOURCE_URL"));
 
 		SpringApplication.run(ProductsApplication.class, args);
 
