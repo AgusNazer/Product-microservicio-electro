cf8278ddd37adef46d41e8b6d12b5182e2cca4ba
agusNazer <agustin.nazer@hotmail.com>
2025-05-13 10:54:49 -0300

    change dotenv for getenv

--- src/main/java/com/agusdev/products/ProductsApplication.java
+++ src/main/java/com/agusdev/products/ProductsApplication.java
@@ -13,13 +13,27 @@ public class ProductsApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-		System.setProperty("SPRING_DATASOURCE_DRIVER_CLASS_NAME", dotenv.get("SPRING_DATASOURCE_DRIVER_CLASS_NAME"));
-		// Verificar que se cargó correctamente
-		System.out.println(dotenv.get("SPRING_DATASOURCE_URL"));
+//		Dotenv dotenv = Dotenv.load();
+//		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+//		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+//		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+//		System.setProperty("SPRING_DATASOURCE_DRIVER_CLASS_NAME", dotenv.get("SPRING_DATASOURCE_DRIVER_CLASS_NAME"));
+//		// Verificar que se cargó correctamente
+//		System.out.println(dotenv.get("SPRING_DATASOURCE_URL"));
+
+		//variables para produccion
+		String datasourceUrl = System.getenv("SPRING_DATASOURCE_URL");
+		String datasourceUsername = System.getenv("SPRING_DATASOURCE_USERNAME");
+		String datasourcePassword = System.getenv("SPRING_DATASOURCE_PASSWORD");
+		String datasourceDriverClassName = System.getenv("SPRING_DATASOURCE_DRIVER_CLASS_NAME");
+
+// Verificar que se cargaron correctamente
+		System.out.println("URL: " + datasourceUrl);
+		System.out.println("Username: " + datasourceUsername);
+		System.out.println("Password: " + datasourcePassword);
+		System.out.println("Driver Class: " + datasourceDriverClassName);
+
+
 
 		SpringApplication.run(ProductsApplication.class, args);
 
