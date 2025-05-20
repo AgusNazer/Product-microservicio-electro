8f9e16a9aadf2bc028ccb9c13443ed47d281a2cc
agusNazer <agustin.nazer@hotmail.com>
2025-05-19 22:21:51 -0300

    cors deploy

--- src/main/java/com/agusdev/products/controller/ProductController.java
+++ src/main/java/com/agusdev/products/controller/ProductController.java
@@ -14,7 +14,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://product-microservicio-electro.onrender.com"
+})
+
 @RestController
 @RequestMapping("/products")
 //Swagger
